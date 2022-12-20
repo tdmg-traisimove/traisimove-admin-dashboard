054132c42691f5dd80a0a8f6666dc625e3f9444c
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-12-20 13:55:04 +1100

    feat: use e-mission-server as the base image of the dashboard.
    Other changes include:
    - restructured the project.

---
+++ docker/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source setup/activate.sh
+
+# change the db host
+echo "DB host = "${DB_HOST}
+if [ -z ${DB_HOST} ] ; then
+    local_host=`hostname -i`
+    sed "s_localhost_${local_host}_" conf/storage/db.conf.sample > conf/storage/db.conf
+else
+    sed "s_localhost_${DB_HOST}_" conf/storage/db.conf.sample > conf/storage/db.conf
+fi
+
+# add test data
+./e-mission-py.bash bin/debug/load_timeline_for_day_and_user.py emission/tests/data/real_examples/shankari_2015-07-22 test_july_22
+
+# run the app
+python app.py
