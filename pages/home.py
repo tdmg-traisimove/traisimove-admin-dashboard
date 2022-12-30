ef9442e184f31d0fe8a5c904cbffffe74484f353
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-12-30 16:22:27 +1100

    feat: add dummy info boxes to the home page. (#1)

--- pages/home.py
+++ pages/home.py
@@ -9,101 +9,61 @@ The workaround is to check if the input value is None.
 import dash
 from dash import dcc, html, Input, Output, State, callback, register_page
 import dash_bootstrap_components as dbc
+from datetime import date
 
 register_page(__name__, path="/")
 
 intro = """
-## Navigation in a callback.
+## Home
 
-With Dash Pages, the routing callback is under the hood, which reduces the amount of boilderplate code you need to write.
-The best way to navigate is to use components such as the `dcc.Link` or `dbc.Button`. When the
- user clicks on these links, it will navigate to the new page without refreshing the page making the navigation
-very fast -- and the best part:  No callback required!.
-
-This works well when you have predefined links. However, at times, you may want to navigate based on an input field, 
-dropdown, or clicking on a figure etc.  In these cases, you can update the link dynamically in a callback.
-
-While it's possible to update the `href` prop of a `dcc.Location` in a callback, this is not recommended because it
- refreshes the page.  
-
-
-### Example 1 
-Using `dcc.Location` to update the url.  **This is not recommended ** since it refreshes the page.  Note the
- `dcc.Location` must be in `app.py`
 """
 
-example2 = """
-### Example 2
-"Using `dcc.Link` and `html.Button` to navigate without refreshing the page"
-"""
+card_icon = {
+    "color": "white",
+    "textAlign": "center",
+    "fontSize": 30,
+    "margin": "auto",
+}
 
-example3 = """
-### Example 3
-Using `dbc.Button` to navigate without refreshing the page
-"""
+def generate_card(title_text, body_text, icon): 
+    card = dbc.Col([
+        dbc.CardGroup([
+            dbc.Card(
+                dbc.CardBody(
+                    [
+                            html.H5(title_text, className="card-title"),
+                            html.P(body_text, className="card-text",),
+                        ]
+                    )
+                ),
+                dbc.Card(
+                    html.Div(className=icon, style=card_icon),
+                    className="bg-primary",
+                    style={"maxWidth": 75},
+                ),
+            ],
+            className="mt-4 shadow")
+    ], md=3)
+    return card
 
+card_users=generate_card("Users (new today)", "100 users (+5)", "fa fa-users")
+card_active_users=generate_card("Active users", "70 users", "fa fa-person-walking")
+card_trips =generate_card("Number of trips", "728 trips", "fa fa-angles-right")
 
 layout = html.Div(
     [
         dcc.Markdown(intro),
-        dbc.Input(
-            id="ticker-search1",
-            placeholder="Search ticker or company name...",
-            className="mb-5",
-            debounce=True,
+        dcc.DatePickerRange(
+                    display_format='D/M/Y',
+                    start_date_placeholder_text='D/M/Y',
+                    end_date_placeholder_text='D/M/Y',
+                    start_date=date(2017, 6, 21)
         ),
-        dcc.Markdown(example2),
-        dcc.Input(
-            id="ticker-search2",
-            placeholder="Search ticker or company name...",
-            className="mb-5",
-        ),
-        dcc.Link(
-            html.Button("submit", n_clicks=0, id="ticker-search2-btn"),
-            id="ticker-search2-link",
-            href="/",
-        ),
-        dcc.Markdown(example3),
-        dbc.InputGroup(
-            [
-                dbc.Input(
-                    id="ticker-search3",
-                    placeholder="Search ticker or company name...",
-                    style={"maxWidth": 400},
-                ),
-                dbc.Button("submit", n_clicks=0, id="ticker-search3-btn", href="/"),
-            ]
-        ),
+        dbc.Row([
+            card_users,
+            card_active_users,
+            card_trips
+        ])
     ]
 )
 
-
-@callback(
-    Output("url", "href"), Input("ticker-search1", "value"), prevent_initial_call=True
-)
-def search(ticker):
-    if ticker is None or ticker == "":
-        return dash.no_update
-    return f"/stocks/{ticker}"
-
-
-@callback(
-    Output("ticker-search2-link", "href"),
-    Input("ticker-search2", "value"),
-    prevent_initial_call=True,
-)
-def search(ticker):
-    if ticker is None or ticker == "":
-        return dash.no_update
-    return f"/stocks/{ticker}"
-
-
-@callback(
-    Output("ticker-search3-btn", "href"),
-    Input("ticker-search3", "value"),
-    prevent_initial_call=True,
-)
-def search(ticker):
-    if ticker is None or ticker == "":
-        return dash.no_update
-    return f"/stocks/{ticker}"
