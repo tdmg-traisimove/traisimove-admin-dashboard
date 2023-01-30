0f63d2e234457b3e869f2cc2686fb617a61e6352
AlirezaRa94 <alirezaraei17@gmail.com>
2023-01-30 15:32:53 +1100

    Added tokens data table

--- pages/tokens.py
+++ pages/tokens.py
@@ -1,17 +1,20 @@
-from dash import dcc, html, Input, Output, callback, register_page
-import dash_table
+from dash import dcc, html, Input, Output, callback, State, register_page, dash_table
 import dash_bootstrap_components as dbc
 
 # Etc
 import pandas as pd
+from datetime import datetime
 from dash.exceptions import PreventUpdate
 
+from emission.storage.decorations.token_queries import insert_many_tokens
 from opadmindash.generate_qr_codes import saveAsQRCode
 from opadmindash.generate_random_tokens import generateRandomTokensForProgram
+import emission.core.get_database as edb
 
 register_page(__name__, path="/tokens")
 
 intro = """## Tokens"""
+QRCODE_PATH = '/home/alireza/WorkSpace/Fourstep/Dashboard/qrcodes'
 
 layout = html.Div(
     [
@@ -46,12 +49,6 @@ layout = html.Div(
             ),
             dbc.Col(
                 [
-                    html.Label('QR Code Directory'),
-                    dcc.Input(value='/', id='token-qrcode-path', type='text', required=True, style={
-                        'font-size': '14px', 'width': '100%', 'display': 'block', 'margin-bottom': '10px',
-                        'margin-right': '5px', 'height': '30px', 'verticalAlign': 'top', 'background-color': '#b4dbf0',
-                        'overflow': 'hidden',
-                    }),
                     html.Label('Out Format'),
                     dcc.Dropdown(options=['url safe', 'hex', 'base64'], value='url safe', id='token-format'),
 
@@ -68,27 +65,26 @@ layout = html.Div(
             ),
         ]),
         html.Div(id='tokens-table'),
-        dcc.Store(id="store-tokens", data={}),
     ]
 )
 
 @callback(
     Output('token-generate', 'n_clicks'),
-    Output('store-tokens', 'data'),
-    Input('token-program', 'value'),
-    Input('token-length', 'value'),
-    Input('token-count', 'value'),
-    Input('token-qrcode-path', 'value'),
-    Input('token-format', 'value'),
+    Output('tokens-table', 'children'),
+    State('token-program', 'value'),
+    State('token-length', 'value'),
+    State('token-count', 'value'),
+    State('token-format', 'value'),
     Input('token-generate', 'n_clicks'),
 )
-def generate_tokens(program, token_length, token_count, qrcode_path, out_format, n_clicks):
-    data = list()
+def generate_tokens(program, token_length, token_count, out_format, n_clicks):
     if n_clicks is not None and n_clicks > 0:
         tokens = generateRandomTokensForProgram(program, token_length, token_count, out_format)
-        for i, token in enumerate(tokens):
-            data.append({'_id': i, 'token': token, 'qr_code': saveAsQRCode(qrcode_path, token)})
-    return 0, {'data': data}
+        insert_many_tokens(tokens)
+        for token in tokens:
+            saveAsQRCode(QRCODE_PATH, token)
+
+    return 0, populate_datatable()
 
 # @callback(
 #     Output('tabs-content', 'children'),
@@ -115,24 +111,38 @@ def generate_tokens(program, token_length, token_count, qrcode_path, out_format,
 #     elif tab == 'tab-tokens-datatable':
 #         pass
 #
-# def populate_datatable(df):
-#     if not isinstance(df, pd.DataFrame):
-#         pass
-#     return dash_table.DataTable(
-#             # id='my-table',
-#             # columns=[{"name": i, "id": i} for i in df.columns],
-#             data=df.to_dict('records'),
-#             export_format="csv",
-#             filter_options={"case": "sensitive"},
-#             # filter_action="native",
-#             sort_action="native",  # give user capability to sort columns
-#             sort_mode="single",  # sort across 'multi' or 'single' columns
-#             page_current=0,  # page number that user is on
-#             page_size=50,  # number of rows visible per page
-#             style_cell={'textAlign': 'left',
-#                         # 'minWidth': '100px',
-#                         # 'width': '100px',
-#                         # 'maxWidth': '100px'
-#                         },
-#             style_table={'overflowX': 'auto',}
-#         )
+def populate_datatable():
+    df = query_tokens()
+    if df.empty:
+        pass
+    df['id'] = df.index + 1
+    df['qr_code'] = "<img src='" + QRCODE_PATH + "/" + df['token'] + ".png' />"
+    df = df.reindex(columns=['id', 'token', 'qr_code'])
+    print(df)
+    return dash_table.DataTable(
+        id='tokens-table',
+        css=[dict(selector="p", rule="margin: 0px;")],
+        columns=[
+            {"id": "id", "name": "id"},
+            {"id": "token", "name": "token"},
+            {"id": "qr_code", "name": "qr_code", "presentation": "markdown"},
+        ],
+        data=df.to_dict('records'),
+        export_format="csv",
+        filter_options={"case": "sensitive"},
+        # filter_action="native",
+        sort_action="native",  # give user capability to sort columns
+        sort_mode="single",  # sort across 'multi' or 'single' columns
+        page_current=0,  # page number that user is on
+        page_size=50,  # number of rows visible per page
+        style_cell={
+            'textAlign': 'left',
+        },
+        markdown_options={"html": True},
+        style_table={'overflowX': 'auto'}
+    )
+
+def query_tokens():
+    query_result = edb.get_token_db().find({}, {"_id": 0})
+    df = pd.json_normalize(list(query_result))
+    return df
