a73c6328d49c9a9dfd526699ff0e5712fff83e6c
AlirezaRa94 <alirezaraei17@gmail.com>
2023-02-17 14:37:49 +1100

    Added study config to environment variables.

--- opadmindash/permissions.py
+++ opadmindash/permissions.py
@@ -1,47 +1,31 @@
 import json
+import os
+
 import requests
 
 from opadmindash.constants import valid_trip_columns, valid_uuids_columns
 
 
-raw_url = "https://raw.githubusercontent.com/"
-config_url = raw_url + "AlirezaRa94/nrel-openpath-deploy-configs/main/configs/admin-dashboard-permissions.nrel-op.json"
-response = requests.get(config_url)
+STUDY_CONFIG = os.getenv('STUDY_CONFIG')
+PATH = "https://raw.githubusercontent.com/AlirezaRa94/nrel-openpath-deploy-configs/main/configs/"
+CONFIG_URL = PATH + STUDY_CONFIG + ".nrel-op.json"
+response = requests.get(CONFIG_URL)
 permissions = json.loads(response.text).get("admin_dashboard", {})
-# permissions = {
-#     "overview_users": True,
-#     "overview_active_users": True,
-#     "overview_trips": True,
-#     "overview_signup_trends": True,
-#     "overview_trips_trend": True,
-#     "data_uuids": True,
-#     "data_trips": True,
-#     "data_trips_columns": [],
-#     "data_uuids_columns": ['update_ts', 'user_id'],
-#     "token_generate": False,
-#     "map_heatmap": True,
-#     "map_bubble": False,
-#     "map_trip_lines": True,
-#     "push_send": False,
-#     "options_uuids": True,
-#     "options_emails": False
-# }
+
 
 def has_permission(perm):
     return True if permissions.get(perm) is True else False
 
 
 def get_trips_columns():
-    columns = list()
-    for column in permissions.get("data_trips_columns", []):
-        if column in valid_trip_columns:
-            columns.append(column)
-    return columns if columns else valid_trip_columns
+    columns = set(valid_trip_columns)
+    for column in permissions.get("data_trips_columns_exclude", []):
+        columns.discard(column)
+    return columns
 
 
 def get_uuids_columns():
-    columns = list()
-    for column in permissions.get("data_uuids_columns", []):
-        if column in valid_uuids_columns:
-            columns.append(column)
-    return columns if columns else valid_uuids_columns
+    columns = set(valid_uuids_columns)
+    for column in permissions.get("data_uuids_columns_exclude", []):
+        columns.discard(column)
+    return columns
