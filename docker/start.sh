30b3a39f88f09b2fb4e87dacd5a01e4237bdb44f
AlirezaRa94 <alirezaraei17@gmail.com>
2023-04-26 10:53:56 +1000

    Removed loading test data into the database.

--- docker/start.sh
+++ docker/start.sh
@@ -10,9 +10,6 @@ else
     sed "s_localhost_${DB_HOST}_" conf/storage/db.conf.sample > conf/storage/db.conf
 fi
 
-# add test data
-./e-mission-py.bash bin/debug/load_timeline_for_day_and_user.py emission/tests/data/real_examples/shankari_2015-07-22 test_july_22
-
 # run the app
 # python app.py
 python app_sidebar_collapsible.py
