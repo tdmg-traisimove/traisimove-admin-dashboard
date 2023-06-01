927817a95a9f7f9c4307f98cbfe2ef02584c047d
Shankari <shankari@eecs.berkeley.edu>
2023-06-01 16:10:38 -0700

    :wrench: Support reading the cognito configuration from environment variables
    This is a partial fix for
    https://github.com/e-mission/op-admin-dashboard/issues/44
    
    We may want to clean this up later - do we really need a `config.py` if
    everything is coming in via environment variables anyway
    
    Testing done:
    - Launched the app
    - Logged in via cognito
    - Saw the admin dashboard

--- config-fake.py
+++ config-fake.py
@@ -1,17 +1,17 @@
 # This is a template for the "config.py" file. Put all of your valid data in this file and then change the name of this
 # file to "config.py".
-
+import os
 
 # Fill these variables when using AWS Cognito authentication; otherwise, keep them empty.
 # All the variables of this class must be filled in using your credential data from your AWS Cognito panel.
 class CognitoConfig:
-    CLIENT_ID = ''
-    CLIENT_SECRET = ''
-    REDIRECT_URL = ''
-    TOKEN_ENDPOINT = ''
-    USER_POOL_ID = ''
-    REGION = ''
-    AUTH_URL = ''
+    CLIENT_ID = os.getenv("COGNITO_CLIENT_ID", '')
+    CLIENT_SECRET = os.getenv("COGNITO_CLIENT_SECRET", '')
+    REDIRECT_URL = os.getenv("COGNITO_REDIRECT_URL", '')
+    TOKEN_ENDPOINT = os.getenv("COGNITO_TOKEN_ENDPOINT", '')
+    USER_POOL_ID = os.getenv("COGNITO_USER_POOL_ID", '')
+    REGION = os.getenv("COGNITO_REGION", '')
+    AUTH_URL = os.getenv("COGNITO_AUTH_URL", '')
 
 
 # Fill this variable when using basic authentication; otherwise, keep it empty.
