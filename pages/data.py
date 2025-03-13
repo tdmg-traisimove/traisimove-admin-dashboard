tdmg-traisimove/traisimove-admin-dashboard

commit f357d6a7235d8b8a045e68063235104a5f090b9b

fix the ordering of columns on data tables
In Python, sets are unique and unordered.
Several of the functions in permissions.py used sets to track the columns that show up, which ensures no duplicates.
However, it causes inconsistent ordering of the columns and is bad UX.
Changing these to lists and only adding columns that aren't already added gives an ordered list without duplicates.

With the order now consistent, I also took this opportunity to ensure that user_token and user_id appear first on any tables that they are present

pages/data.py
@@ -191,9 +191,8 @@ def render_content(tab, store_uuids, store_excluded_uuids, store_trips, store_de
                 logging.debug(f"Callback - {selected_tab} Stage 2: Handling Trips tab.")
 
                 data = store_trips.get("data", [])
-                columns = perm_utils.get_allowed_trip_columns()
-                columns.update(col['label'] for col in perm_utils.get_allowed_named_trip_columns())
-                columns.update(store_trips.get("userinputcols", []))
+                columns = perm_utils.get_all_trip_columns()
+                columns.extend(store_trips.get("userinputcols", []))
                 has_perm = perm_utils.has_permission('data_trips')
 
                 df = pd.DataFrame(data)
@@ -428,9 +427,9 @@ def populate_datatable(df, store_uuids, table_id):
             uuids_df = pd.DataFrame(store_uuids['data'])
             user_id_col = 'data.user_id' if 'data.user_id' in df.columns else 'user_id'
             if user_id_col in df.columns:
-                user_id_token_map = uuids_df.set_index(user_id_col)['user_token'].to_dict()
+                user_id_token_map = uuids_df.set_index('user_id')['user_token'].to_dict()
                 df.insert(
-                    uuids_df.columns.get_loc('user_id'),
+                    df.columns.get_loc(user_id_col),
                     'user_token',
                     df[user_id_col].map(user_id_token_map)
                 )

utils/constants.py
@@ -9,6 +9,7 @@ MULTILABEL_NAMED_COLS = [
 ]
 
 VALID_TRIP_COLS = [
+    "user_id",
     "data.start_local_dt",
     "data.start_fmt_time",
     "data.end_local_dt",
@@ -24,7 +25,6 @@ VALID_TRIP_COLS = [
     "data.primary_sensed_mode",
     "data.primary_predicted_mode",
     "data.primary_ble_sensed_mode",
-    "user_id"
 ]
 
 BINARY_TRIP_COLS = [

utils/permissions.py
@@ -47,8 +47,8 @@ def get_allowed_named_trip_columns():
         return permissions.get('additional_trip_columns', [])
 
 def get_required_columns():
-    required_cols = {'user_id'}
-    required_cols.update(col['path'] for col in constants.REQUIRED_NAMED_COLS)
+    required_cols = ['user_id']
+    required_cols.extend(col['path'] for col in constants.REQUIRED_NAMED_COLS)
     return required_cols
 
 
@@ -60,47 +60,41 @@ def get_all_named_trip_columns():
     return named_columns
 
 def get_all_trip_columns():
-    columns = set()
-    # logging.debug("get_all_trip_columns: curr set is %s" % columns)
-    columns.update(get_allowed_trip_columns())
-    # logging.debug("get_all_trip_columns: curr set is %s" % columns)
-    columns.update(
-        col['path'] for col in get_allowed_named_trip_columns()
-    )
-    # logging.debug("get_all_trip_columns: curr set is %s" % columns)
-
-    columns.update(get_required_columns())
-    # logging.debug("get_all_trip_columns: curr set is %s" % columns)
-    columns.update(permissions.get('additional_trip_columns', []))
+    columns = get_allowed_trip_columns()
+    columns.extend([col['path'] for col in get_allowed_named_trip_columns()
+                    if col['path'] not in columns])
+    columns.extend([col for col in get_required_columns()
+                    if col not in columns])
     logging.debug("get_all_trip_columns: after additional columns, curr set is %s" % columns)
     return columns
 
 
 def get_allowed_trip_columns():
-    columns = set(constants.VALID_TRIP_COLS)
+    columns = list(constants.VALID_TRIP_COLS)
     for column in permissions.get("data_trips_columns_exclude", []):
-        columns.discard(column)
+        columns.remove(column)
     for column in permissions.get("additional_trip_columns", []):
-        columns.add(column)
+        if column not in columns:
+            columns.append(column)
     logging.debug("allowed trip columns are %s" % columns)
     return columns
 
 
 def get_uuids_columns():
-    columns = set(constants.VALID_UUIDS_COLS)
+    columns = list(constants.VALID_UUIDS_COLS)
     for column in permissions.get("data_uuids_columns_exclude", []):
-        columns.discard(column)
+        columns.remove(column)
     return columns
 
 def get_demographic_columns(columns):
     for column in permissions.get("data_demographics_columns_exclude", []):
-        columns.discard(column)
+        columns.remove(column)
     return columns
 
 def get_trajectories_columns(columns):
-    columns = set(columns)
+    columns = list(columns)
     for column in permissions.get("data_trajectories_columns_exclude", []):
-        columns.discard(column)
+        columns.remove(column)
     return columns
 
 def get_token_prefix():

commit 0743a8ccb30568124493674158334e1017a5ef8c

fix excluded columns showing on trips table
We should only show "allowed_trip_columns" + userinputcols, not "all_trip_columns"
Regression from f357d6a7235d8b8a045e68063235104a5f090b9b

pages/data.py
@@ -196,7 +196,7 @@ def render_content(tab, store_uuids, store_excluded_uuids, store_trips, store_de
                 logging.debug(f"Callback - {selected_tab} Stage 2: Handling Trips tab.")
 
                 data = store_trips.get("data", [])
-                columns = perm_utils.get_all_trip_columns()
+                columns = perm_utils.get_allowed_trip_columns()
                 columns.extend(store_trips.get("userinputcols", []))
                 has_perm = perm_utils.has_permission('data_trips')
 